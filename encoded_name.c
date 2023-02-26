98058cdc1856550e87538db2091f7ea9a3c15397
Anoop Kumar <113820345+Anoopkumargithub@users.noreply.github.com>
2023-02-26 19:41:20 +0530

    Create encoded_name.c

---
+++ encoded_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a,b,c,d,e;
+    scanf("%d&%d&%d&%d&%d",&a,&b,&c,&d,&e);
+    printf("%c%c%c%c%c",a,b,c,d,e);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
