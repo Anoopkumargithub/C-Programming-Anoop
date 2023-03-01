98b3d9d9d4554548af109dc7b7ad654b85a980d7
Anoop Kumar <113820345+Anoopkumargithub@users.noreply.github.com>
2023-03-01 09:39:21 +0530

    Update vowels.c

--- vowels.c
+++ vowels.c
@@ -1,7 +1,26 @@
 #include<stdio.h>
+#include <ctype.h>
 int main()
 {
-    char a,e,i,;
-    printf("enter any number");
-    scan("%c",&a);
+    char a,b;
+    scanf("%c",&b);
+    a = tolower(b);
+    // if else
+    if (a=='a' || a=='e' || a=='i' || a=='o' || a=='u'){
+        printf("Vowel");
+    }else{
+        printf("Consonant");
+    }
+    // ternary operator
+    (a=='a' || a=='e' || a=='i' || a=='o' || a=='u')?printf("Vowel"):printf("Consonant");
+    // switch 
+    switch (a=='a' || a=='e' || a=='i' || a=='o' || a=='u')
+    {
+    case 1:
+        printf("Vowels");
+        break;
+    case 0:
+        printf("consonant");
+        break;
+    }
 }
