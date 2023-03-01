1867eca3525c22ed173ef0f7d2bc1bc5ee07810c
Anoop Kumar <113820345+Anoopkumargithub@users.noreply.github.com>
2023-03-01 20:20:18 +0530

    Create hello_me.c

---
+++ hello_me.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char a[100];
+    scanf("%s", a);
+    printf("Welcome %s",a);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
