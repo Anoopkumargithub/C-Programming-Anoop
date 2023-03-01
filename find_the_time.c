de30f010cb4a64bd1ecb759f3de4ddf9e2b759df
Anoop Kumar <113820345+Anoopkumargithub@users.noreply.github.com>
2023-03-01 20:21:51 +0530

    Create find_the_time.c

---
+++ find_the_time.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    float r,t;
+    scanf("%f",&r);
+    t = (1/r);
+    printf("%.2f",t*100);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
