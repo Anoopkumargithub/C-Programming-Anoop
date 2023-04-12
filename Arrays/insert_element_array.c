16ae51097f2a4681b3d3a660ada1b41f6c75cdd3
Anoop Kumar <113820345+Anoopkumargithub@users.noreply.github.com>
2023-04-12 22:08:55 +0530

    Add files via upload

---
+++ Arrays/insert_element_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Enter no. of element ");
+    scanf("%d",&n);
+    int a[n],pos,ele;
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[n]);
+    printf("Element and position");
+    scanf("%d %d",&ele,&pos);
+    }
+    for (int i=n-1;i>=pos;i++){
+        // for
+    }
+    return 0;
+}
---
+++ Arrays/read_print_array.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a[] = {1,2,3,5,7,9,0};
+    for(int i=0;i<=6;i++){
+        printf("%d %d",i,a[i]);
+        printf("\n");
+    }
+    return 0;
+}
